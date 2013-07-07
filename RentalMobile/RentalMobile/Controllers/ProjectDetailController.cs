b3ace00910ab27dc528dc4cf85fd9c27309f06b7
Haithem <haithemaraissia@gmail.com>
2013-07-06 21:01:30 -0700

    Still in Project Detail
    more work need to be done on it.

--- RentalMobile/RentalMobile/Controllers/ProjectDetailController.cs
+++ RentalMobile/RentalMobile/Controllers/ProjectDetailController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -21,7 +22,99 @@ namespace RentalMobile.Controllers
             return View(db.ProjectPhotoes.ToList());
         }
 
-      
+
+
+
+        [HttpPost]
+        public ActionResult MakeOffer(FormCollection form)
+        {
+            TempData["startDate"] = form["HiddenStartDate"];
+            TempData["endDate"] = form["HiddenEndDate"];
+            TempData["ProjectId"] = form["HiddenProjectId"];
+            return RedirectToAction("Confirm");
+        }
+
+
+
+        //
+        // GET: /RATest/Edit/5
+
+        public ActionResult Confirm()
+        {
+            ViewBag.StartDate = TempData["startDate"];
+            ViewBag.EndDate = TempData["endDate"];
+            ViewBag.ProjectID = TempData["ProjectId"];
+
+            return View();
+        }
+
+
+
+        
+
+        [HttpPost]
+        public ActionResult Confirm(FormCollection form)
+        {
+
+            //SpecialistID
+            var specialistID = (Convert.ToInt32( form["HiddenProjectId"]));
+            var specialist = db.Specialists.FirstOrDefault(t => t.SpecialistId == specialistID);
+            if (specialist == null)
+            {
+                return RedirectToAction("MakeOffer");
+            }
+
+
+
+//Make sure Project is still Open, not closed by Owner, Agent or finished.
+
+
+            //Offer Data
+            //More Work
+            //You need to put them in the view so you can grab them
+            var startdate = form["StartDate"];
+            var enddate = form["StartDate"];
+            var amount = form["Amount"];
+            var currency = form["Currency"];
+            var quicknote = form["Quick Note"];
+
+
+
+
+            //Insert into Owner, Agent Offer
+            //Insert into Sepcialist submittted ofer
+            //Do the confirmation.
+
+
+            ////WHEN AMAZON PAYMENT SUCCEED
+            //if (property != null)
+            //{
+            //    var posterrole = property.PosterRole.Trim();
+
+            //    switch (posterrole)
+            //    {
+
+            //        case "Owner":
+            //            //Insert into Pending Application
+            //            if (property.PosterID != null)
+            //                InsertOwnerPendingApplication(tenantrentalapplication.First(), (int)property.PosterID);
+            //            ViewData["confirmationmsg"] = "Your Application had been succesfully submitted to the Owner.";
+
+            //            //Confirmation that is has been posted
+            //            break;
+
+            //        case "Agent":
+            //            //Insert into Pending Application
+            //            if (property.PosterID != null)
+            //                InsertAgentPendingApplication(tenantrentalapplication.First(), (int)property.PosterID);
+            //            ViewData["confirmationmsg"] = "Pass";
+            //            break;
+
+            //    }
+            //}
+            return View();
+        }
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
