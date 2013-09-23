186789b66c869399e2d020bec2c1a7f84da5fc95
Haithem <haithemaraissia@gmail.com>
2013-09-22 23:25:33 -0700

    UI For Add / Remove Team

---
+++ RentalMobile/RentalMobile/Controllers/AddTeamMemberController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentalMobile.Models;
+
+namespace RentalMobile.Controllers
+{ 
+    public class AddTeamMemberController : Controller
+    {
+        private DB_33736_rentalEntities db = new DB_33736_rentalEntities();
+
+        //
+        // GET: /AddTeamMember/
+
+        public ViewResult Index()
+        {
+            return View(db.Specialists.ToList());
+        }
+
+        //
+        // GET: /AddTeamMember/Create
+
+        public ActionResult Submit()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /AddTeamMember/Create
+
+        [HttpPost]
+        public ActionResult Submit(Specialist specialist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Specialists.Add(specialist);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(specialist);
+        }
+        
+        //
+        // GET: /AddTeamMember/Edit/5
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RentalMobile/RentalMobile/Controllers/RemoveTeamMemberController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentalMobile.Models;
+
+namespace RentalMobile.Controllers
+{ 
+    public class RemoveTeamMemberController : Controller
+    {
+        private DB_33736_rentalEntities db = new DB_33736_rentalEntities();
+
+        //
+        // GET: /RemoveTeamMember/
+
+        public ViewResult Index()
+        {
+            return View(db.Specialists.ToList());
+        }
+
+
+        //
+        // GET: /RemoveTeamMember/Create
+
+        public ActionResult Submit()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /RemoveTeamMember/Create
+
+        [HttpPost]
+        public ActionResult Submit(Specialist specialist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Specialists.Add(specialist);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(specialist);
+        }
+        
+ 
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
