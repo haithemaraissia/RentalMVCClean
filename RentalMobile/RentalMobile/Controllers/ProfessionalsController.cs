c14075fd3816dcb6d8cfa4f2a83d140e545df22a
haithemaraissia <haithemaraissia@gmail.com>
2014-02-07 15:09:59 -0800

    Specialist Coverage Update

--- RentalMobile/RentalMobile/Controllers/ProfessionalsController.cs
+++ RentalMobile/RentalMobile/Controllers/ProfessionalsController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using RentalMobile.Helpers;
+using RentalMobile.ModelViews;
 using RentalMobile.Models;
 
 namespace RentalMobile.Controllers
@@ -16,6 +18,72 @@ namespace RentalMobile.Controllers
             ViewBag.SpecialistGoogleMap = specialist.GoogleMap;
             return View(specialist);
         }
+
+
+
+        public PartialViewResult _Coverage(int id)
+        {
+
+            if (id != 0)
+            {
+                const int specialistrole = 1;
+                var lookUp =
+                    _db.MaintenanceCompanyLookUps.FirstOrDefault(
+                        x => x.Role == specialistrole && x.UserId == id);
+                if (lookUp != null)
+                {
+                    int companyId = lookUp.CompanyId;
+
+                    var mp = new SpecialistMaintenanceProfile
+                    {
+                        MaintenanceCompanyLookUp = _db.MaintenanceCompanyLookUps.Find(companyId),
+                        MaintenanceCompany = _db.MaintenanceCompanies.Find(companyId),
+                        MaintenanceCompanySpecialization = _db.MaintenanceCompanySpecializations.Find(companyId),
+                        MaintenanceCustomService = _db.MaintenanceCustomServices.Find(companyId),
+                        MaintenanceExterior = _db.MaintenanceExteriors.Find(companyId),
+                        MaintenanceInterior = _db.MaintenanceInteriors.Find(companyId),
+                        MaintenanceNewConstruction = _db.MaintenanceNewConstructions.Find(companyId),
+                        MaintenanceRepair = _db.MaintenanceRepairs.Find(companyId),
+                        MaintenanceUtility = _db.MaintenanceUtilities.Find(companyId),
+                    };
+
+                    return PartialView(mp);
+                }
+            }
+            return null;
+        }
+
+
+        public PartialViewResult _Description(int id)
+        {
+
+            if (id != 0)
+            {
+                const int specialistrole = 1;
+                var lookUp =
+                    _db.MaintenanceCompanyLookUps.FirstOrDefault(
+                        x => x.Role == specialistrole && x.UserId == id);
+                if (lookUp != null)
+                {
+                    int companyId = lookUp.CompanyId;
+
+                    var mcs = _db.MaintenanceCompanySpecializations.FirstOrDefault(x => x.CompanyId == companyId);
+                    if (mcs != null)
+                    {
+                        ViewBag.Rate = mcs.Rate;
+                        var currency = _db.Currencies.FirstOrDefault(x => x.CurrencyID == mcs.CurrencyID);
+                        if (currency != null)
+                            ViewBag.Currency = currency.CurrencyValue;
+                    }
+
+                    var currentspecialist = _db.Specialists.FirstOrDefault(x => x.SpecialistId == id);
+                    return PartialView(currentspecialist);
+                }
+            }
+            return null;
+        }
+
+
 
         protected override void Dispose(bool disposing)
         {
